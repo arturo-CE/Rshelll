c5d3d5dfeea913f44b5b91c3a42420d44e720025
arturo-CE <61763318+arturo-CE@users.noreply.github.com>
2020-04-17 21:50:00 -0700

    Create all_tests.sh

---
+++ intergration_tests/all_tests.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+echo
+echo
+echo "Single Commands: "
+echo
+
+./single_command_tests.sh
+
+echo
+echo
+echo "Multiple Commands: "
+echo
+
+./multiple_commands_tests.sh
+
+echo
+echo
+echo "Commented Commands: "
+echo
+
+./commented_command_tests.sh
+
+echo
+echo
+echo "Exit Commands: "
+echo
+
+./exit_command_tests.sh
+
+echo
+echo
+echo "Precedence Comamnds: "
+echo
+
+./precedence_tests.sh
+
+echo
+echo
+echo "Test Literals: "
+echo
+
+./test_literal_tests.sh
+
+echo
+echo
+echo "Test Symbolics: "
+echo
+
+./test_symbolic_tests.sh
+
+echo
+echo
