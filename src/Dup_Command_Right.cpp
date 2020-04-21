88cc39d58cb95822d7c0e987cc48830d2fb5d157
arturo-CE <61763318+arturo-CE@users.noreply.github.com>
2020-04-21 00:11:20 -0700

    Create Dup_Command_Right.cpp

---
+++ src/Dup_Command_Right.cpp
@@ -0,0 +1,48 @@
+#include "Dup_Command_Right.hpp"
+
+Dup_Command_Right::Dup_Command_Right(char* foo){
+	this->arg = foo;
+
+}
+
+bool Dup_Command_Right::execute(Redirection_Command* foo, int*bar){
+	pid_t pid, wait_pid;
+		int status;
+    
+		pid = fork();
+			if(pid == -1){
+				perror("fork failed.");
+				//commandSuccess = false;
+				exit(EXIT_FAILURE);
+			}
+			else if(pid == 0){
+				int fd = open(getArg(), O_TRUNC | O_WRONLY);
+				if(fd < 0){perror("Cannot open file."); return false;}
+				string line = "";
+				int cd = dup(fd);
+				if(cd < 0){perror("Dup failed."); return false;}
+
+				ifstream ifs( foo->getArg() );
+
+				while(ifs){
+					getline(ifs, line);
+					write(fd, line.c_str(), line.length());
+				}
+				close(cd);
+				close(fd);
+				ifs.close();
+			}
+			else{
+				wait_pid = waitpid(pid,&status,WUNTRACED | WCONTINUED);
+				if(wait_pid == -1){
+					perror("wait_pid");
+					exit(EXIT_FAILURE);
+			}
+			else{
+			  while(!WIFEXITED(status) && !WIFSIGNALED(status)){
+						wait_pid = waitpid(pid,&status, WUNTRACED | WCONTINUED);
+					}
+			}
+		}
+		return true;
+}
