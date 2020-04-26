afa6b956d40b6499e1f03e09b486f8c63a8c2aad
arturo-CE <61763318+arturo-CE@users.noreply.github.com>
2020-04-25 20:45:47 -0700

    Create Parse_Execvp.hpp

---
+++ src/Parse_Execvp.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+#include <vector> 
+#include <ctype.h>
+#include <stack>
+#include <sstream>
+using namespace std;
+
+class Parse_Execvp{
+	public:
+		char** cStringify(string);
+		string charToString(char**);
+		vector<char**> getArgs();
+		stack< pair<char,string> > getCommands(string);
+		bool isConnectingSymbol(char);
+		bool isSurroundingSymbol(char);
+		bool checkForSpaces(string);
+		string removeComments(string foo);
+		string convertTestString(string);
+		string trimEdgeSpaces(string);
+};
